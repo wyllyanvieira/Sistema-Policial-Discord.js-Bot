6bd42aa1f01928fbcb1a314bfacca146c46b037f
Wyllyam Vieira <wyllyambr80@gmail.com>
2024-01-24 09:53:27 -0400

    🛠️Fix ranking removendo usuario que sairam da guild

--- Commands/Geral/ranking.js
+++ Commands/Geral/ranking.js
@@ -6,7 +6,7 @@ const db = new sqlite3.Database('pontos.db'); // Use o mesmo nome do banco de da
 
 module.exports = {
   name: "ranking",
-  description: "[🦅 Utilidade] Veja o ping do botranking do pessoal com ponto batido.",
+  description: "[👑 Moderação] Veja o ranking dos usuarios com mais bateponto aberto.",
   type: Discord.ApplicationCommandType.ChatInput,
 
   run: async (client, interaction) => {
@@ -14,10 +14,10 @@ module.exports = {
       const horas = Math.floor(segundos / 3600);
       const minutos = Math.floor((segundos % 3600) / 60);
       const segundosRestantes = segundos % 60;
-
+  
       return `${horas}h ${minutos}m ${segundosRestantes.toFixed(0)}s`;
     }
-
+  
     function calcularRanking(rows) {
       const ranking = rows.map(row => {
         const intervalosArray = JSON.parse(row.intervalos); // Converte a string JSON para um array
@@ -25,45 +25,52 @@ module.exports = {
         const tempoAbertoAtual = row.aberto ? (new Date() - new Date(row.aberto)) / 1000 : 0;
         return { idUsuario: row.usuario_id, tempoTotal: tempoTotal + tempoAbertoAtual };
       });
-
+  
       ranking.sort((a, b) => b.tempoTotal - a.tempoTotal);
-
+  
       return ranking;
     }
-
+  
     // Obtém os dados do banco de dados
     db.all('SELECT * FROM pontos', async (err, rows) => {
       if (err) {
         console.error(err);
         return;
       }
-    if (!interaction.member.permissions.has("MANAGE_MESSAGES")) {
+      
+      if (!interaction.member.permissions.has("MANAGE_MESSAGES")) {
         interaction.reply({ content: `<:icons_Wrong75:1198037616956821515> | Você não possui permissão para utilizar este comando.`, ephemeral: true })
-    } else {
-      const ranking = calcularRanking(rows);
-      var d = new Date();
-      let resposta = `# Ranking de Tempo de Ponto: \n- Solicitado por: ${interaction.user} \n- Data: ${d} \n\n`;
-      for (let i = 0; i < ranking.length; i++) {
-        const user = ranking[i];
-        try {
-          const membro = await interaction.guild.members.fetch(user.idUsuario);
-          const nomeUsuario = membro ? membro.displayName : 'Usuário desconhecido';
-          const tempoFormatado = formatarTempo(user.tempoTotal);
-
-          let emoji = '';
-          if (i === 0) emoji = '🥇 ';
-          else if (i === 1) emoji = '🥈 ';
-          else if (i === 2) emoji = '🥉 ';
-          else if (i >= 2) emoji = '🎖️ ';
-
-          resposta += `${emoji}${membro}: ${tempoFormatado}\n`;
-        } catch (error) {
-          resposta += `Usuário desconhecido (ID: ${user.idUsuario}): ${formatarTempo(user.tempoTotal)}\n`;
+      } else {
+        const ranking = calcularRanking(rows);
+        var d = new Date();
+        let resposta = `# Ranking de Tempo de Ponto: \n- Solicitado por: ${interaction.user} \n- Data: ${d} \n\n`;
+  
+        for (let i = 0; i < ranking.length; i++) {
+          const user = ranking[i];
+  
+          try {
+            const membro = await interaction.guild.members.fetch(user.idUsuario);
+  
+            // Verifica se o membro existe antes de adicionar à resposta
+            if (membro) {
+              const nomeUsuario = membro.displayName;
+              const tempoFormatado = formatarTempo(user.tempoTotal);
+  
+              let emoji = '';
+              if (i === 0) emoji = '`🥇` ';
+              else if (i === 1) emoji = '`🥈` ';
+              else if (i === 2) emoji = '`🥉` ';
+              else if (i >= 2) emoji = '`🎖️` ';
+  
+              resposta += `${emoji}${nomeUsuario}: ${tempoFormatado}\n`;
+            }
+          } catch (error) {
+            console.error(error);
+          }
         }
+  
+        interaction.reply({ content: resposta });
       }
-
-      interaction.reply({ content: resposta});
-    }
     });
-  },
+  },  
 };
